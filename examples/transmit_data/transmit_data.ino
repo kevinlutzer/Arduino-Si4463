668f0fe25170784389a4b0522c0247367c651dc6
Kevin Lutzer <kevinlutzer@proton.me>
2025-09-14 17:09:35 -0400

    WIP

---
+++ examples/transmit_data/transmit_data.ino
@@ -0,0 +1,84 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Si4463.h>
+
+//
+// Pins
+// 
+
+#define LED 25 // Built-in LED on Raspberry Pi Pico
+#define SDN 16 // System reset for the SI4463 chip. Active low
+#define IRQ 17 // IRQ Pin from the SI4463 to the Pi
+
+#define SCK 10
+#define MOSI 11
+#define MISO 12 
+#define CS 13
+
+Si4463 radio = Si4463(&SPI1, CS, SDN, IRQ);
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(SDN, OUTPUT);
+	pinMode(IRQ, INPUT);
+
+  // Disable the module on boot
+  digitalWrite(SDN, HIGH);
+
+  // Initialize the SPI
+  SPI1.setRX(MISO);
+  SPI1.setCS(CS);
+  SPI1.setSCK(SCK);
+  SPI1.setTX(MOSI);
+
+  Serial.begin(115200); // Set baud rate
+  while (!Serial);   
+  Serial.println("Serial started");
+
+  // Init the module
+  radio.begin();
+  radio.powerOnReset();
+
+  delay(5000);
+
+  // heck if RF4463 works
+	if(!radio.checkDevice()){
+    Serial.println("Failed");
+	} else {
+    Serial.println("Success");
+  }
+
+  // radio.setProperties(0x0000, 2, (uint8_t[]){0x40, 0x00}); // GLOBAL_XO_TUNE_2
+
+  // set antenna switch,in RF4463 is GPIO2 and GPIO3
+	// don't change setting of GPIO2,GPIO3,NIRQ,SDO
+  uint8_t buf[6];
+  uint8_t buf_rx[6];
+
+	buf[0]  = RF4463_GPIO_NO_CHANGE; 
+	buf[1]  = RF4463_GPIO_NO_CHANGE;
+	buf[2]  = RF4463_GPIO_RX_STATE;
+	buf[3]  = RF4463_GPIO_TX_STATE;
+	buf[4]  = RF4463_NIRQ_INTERRUPT_SIGNAL; 
+	buf[5]  = RF4463_GPIO_SPI_DATA_OUT; 
+
+	if (!radio.setCommand(6,RF4463_CMD_GPIO_PIN_CFG,buf)) {
+    Serial.println("Failed to send command");
+  }
+
+  if (!radio.setCommand(6, RF4463_CMD_GPIO_PIN_CFG, buf_rx)) {
+    Serial.println("Failed to get command");
+  }
+
+  for (int i = 0; i < 6; i ++) {
+    Serial.printf("Buf[%d] %d, %d \n", i, buf[i], buf_rx[i]);
+  }
+
+}
+
+void loop() {
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+}
---
+++ src/Si4463.cpp
@@ -0,0 +1,127 @@
+#include <Arduino.h>
+#include <SPI.h>
+
+#include "Si4463.h"
+
+Si4463::Si4463(SPIClassRP2040 * spi, pin_size_t cs, pin_size_t sdn, pin_size_t irq) {
+    _spi = spi;
+    _cs = cs;
+    _sdn = sdn;
+    _irq = irq;
+}
+
+void Si4463::begin()
+{
+    pinMode(_cs, OUTPUT);
+    pinMode(_sdn, OUTPUT);
+	pinMode(_irq, INPUT);
+
+    digitalWrite(_sdn, HIGH);
+
+    _spi->begin(true);
+    _spi->beginTransaction(SPISettings(32768, MSBFIRST, SPI_MODE0));
+}
+
+void Si4463::powerOnReset()
+{
+  digitalWrite(_sdn, LOW);
+  delay(1000);						// wait for RF4463 stable
+
+	// send power up command
+  uint8_t tx_buf[]={0x02, 0x01, 0x00, 0x01, 0xC9, 0xC3, 0x80};
+  _spi->transfer(tx_buf, 7);
+
+  delay(200);
+}
+
+bool Si4463::checkCTS()
+{
+  uint16_t timeOutCnt;
+	timeOutCnt=RF4463_CTS_TIMEOUT;
+
+  Serial.println("Send request");
+
+  uint8_t rx = 0;
+  uint16_t count = 0;
+  do {
+    digitalWrite(_cs, LOW);
+    rx = _spi->transfer(RF4463_CMD_READ_BUF);
+    count ++;
+  } while ( rx != RF4463_CTS_REPLY && count < RF4463_CTS_TIMEOUT );
+
+  Serial.printf("RX Value received %d after count %d", rx, count);
+
+  return rx == RF4463_CTS_REPLY;
+}
+
+bool Si4463::getCommand(uint8_t length,uint8_t command, uint8_t * paraBuf) {
+
+    _spi->transfer(command);				// set command to read 
+    
+    // check if RF4463 is ready 
+    if(!checkCTS())	{
+        return false;  
+    }
+  
+    uint8_t * _txbuf = (uint8_t * ) malloc(length * sizeof(uint8_t));
+    memset(_txbuf, 0x00, length);
+    _txbuf[0] = RF4463_CMD_READ_BUF;
+
+    _spi->transfer(_txbuf, paraBuf, length);		// read parameters
+    digitalWrite(_cs, HIGH);
+
+    free(_txbuf);
+    return true;
+}
+
+bool Si4463::checkDevice()
+{
+	uint8_t buf[9];
+	uint16_t partInfo;
+
+	getCommand(9, RF4463_CMD_PART_INFO, buf);		// read part info to check if 4463 works
+		
+  for (int i = 0; i < 9; i ++) {
+    Serial.printf("Buf[%d]=%d\n", i, buf[i]);
+  }
+
+	partInfo=buf[1]<<8|buf[2];
+	return partInfo == 0x4463;
+}
+
+bool Si4463::setProperties(uint16_t startProperty, uint8_t length ,uint8_t* paraBuf)
+{
+	uint8_t buf[4];
+
+	if(!checkCTS())
+		return false;
+
+    uint8_t * tx_buf = (uint8_t * ) malloc((length + 4) * sizeof(uint8_t));
+    tx_buf[0] = RF4463_CMD_SET_PROPERTY;
+    tx_buf[1] = startProperty >> 8;   		// GROUP
+    tx_buf[2] = length;                	// NUM_PROPS
+    tx_buf[3] = startProperty & 0xff; 		// START_PROP
+
+    memcpy(tx_buf + 4, paraBuf, length);
+
+    _spi->transfer(tx_buf, length + 4);
+
+    free(tx_buf);
+
+	return true;
+}
+
+bool Si4463::setCommand(uint8_t length,uint8_t command,uint8_t* paraBuf)
+{
+	if(!checkCTS())
+		return false;
+
+    uint8_t * tx_buf = (uint8_t * ) malloc((length + 1) * sizeof(uint8_t));
+    tx_buf[0] = command;   		// COMMAND
+    memcpy(tx_buf + 1, paraBuf, length);
+
+    _spi->transfer(tx_buf, length + 1);
+
+    free(tx_buf);
+	return true;
+}
